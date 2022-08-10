8732f0cf1c7d2211477421e8770fa3ab956fda54
angelique KRAUSE <krause_angelique@hotmail.com>
2022-08-10 15:50:57 +0200

    script

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
-console.log("Salut");
 const outputList = document.getElementById("output");
-console.log(outputList);
-const fetchButton = document.getElementById
+const fetchButton = document.getElementById("fetching");
 
-
-// function
 function fetchingDatas(method, url, data) {
-    //"https://jsonplaceholder.typicode.com/posts"
-    fetch(url, {
-        // 'GET'
+    return fetch(url, {
         method: method,
         body: JSON.stringify(data),
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
     })
         .then((response) => {
-            if (response.status >= 200 & response.status < 300) {
+            if (response.status >= 200 && response.status < 300) {
                 return response.json();
             } else {
                 return response.json().then((errData) => {
                     console.log(errData);
-                    throw new Error("Semehting went wrong ");
+                    throw new Error("Something went wrong - server-side");
                 });
             }
         })
-
         .catch((error) => {
             console.log(error);
             throw new Error("Something went wrong.");
         });
 }
 
-// Call the function
-fetchingDatas(data);
-
 async function fetchPosts() {
     try {
-        const responseDat = await fetchingDatas(
-            'GET',
-            'https://jsonplaceholder.typicode.com/posts'
+        const listOfPosts = await fetchingDatas(
+            "GET",
+            "https://jsonplaceholder.typicode.com/posts"
         );
-
-        outputList
+        for (const post of listOfPosts) {
+            const postElement = document.importNode(
+                document.getElementById("templatePost").content,
+                true
+            );
+            postElement.querySelector(".title").textContent = post.title; // <p></p>
+            postElement.querySelector(".description").textContent = post.body;
+            outputList.appendChild(postElement);
+        }
     } catch (error) {
         console.log(error.message);
     }
